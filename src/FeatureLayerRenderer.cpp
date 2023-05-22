1ed1254ab5571f6f23c8a350223af9c397ebb753
Laura Lahesoo <l.lahesoo@klebert-engineering.de>
2023-05-22 16:15:04 +0200

    Code formatting.

--- src/FeatureLayerRenderer.cpp
+++ src/FeatureLayerRenderer.cpp
@@ -1,26 +1,29 @@
-#include <vector>
 #include <string>
+#include <vector>
 
-#include "tiny_gltf.h"
 #include "duckfile.c"
+#include "tiny_gltf.h"
 
 #include "include/FeatureLayerRenderer.h"
 
 FeatureLayerRenderer::FeatureLayerRenderer() : r_() {}
 
-const RenderObject& FeatureLayerRenderer::render() {
-  return r_;
+const RenderObject& FeatureLayerRenderer::render()
+{
+    return r_;
 }
 
-RenderObject::RenderObject() {
-  glb.assign(std::begin(duckfile), std::end(duckfile));
+RenderObject::RenderObject()
+{
+    glb.assign(std::begin(duckfile), std::end(duckfile));
 }
 
-uint32_t RenderObject::getGlbSize() {
-  return duckfile_len;
+uint32_t RenderObject::getGlbSize()
+{
+    return duckfile_len;
 }
 
-__UINT64_TYPE__ RenderObject::getGlbPtr() {
-  return reinterpret_cast<__UINT64_TYPE__>(glb.data());
+__UINT64_TYPE__ RenderObject::getGlbPtr()
+{
+    return reinterpret_cast<__UINT64_TYPE__>(glb.data());
 }
-
--- src/include/FeatureLayerRenderer.h
+++ src/include/FeatureLayerRenderer.h
@@ -3,32 +3,35 @@
 
 #include <emscripten/bind.h>
 
-class RenderObject {
+class RenderObject
+{
 public:
-  RenderObject();
-  uint32_t getGlbSize();
-  __UINT64_TYPE__ getGlbPtr();
+    RenderObject();
+    uint32_t getGlbSize();
+    __UINT64_TYPE__ getGlbPtr();
+
 private:
-  std::vector<uint8_t> glb;
+    std::vector<uint8_t> glb;
 };
 
-
-class FeatureLayerRenderer {
+class FeatureLayerRenderer
+{
 public:
-  FeatureLayerRenderer();
-  const RenderObject& render();
+    FeatureLayerRenderer();
+    const RenderObject& render();
+
 private:
-  RenderObject r_;
+    RenderObject r_;
 };
 
-
-EMSCRIPTEN_BINDINGS(FLTest) {
-  emscripten::class_<FeatureLayerRenderer>("FeatureLayerRenderer")
-      .constructor()
-      .function("render", &FeatureLayerRenderer::render);
-  emscripten::class_<RenderObject>("RenderObject")
-      .function("getGlbSize", &RenderObject::getGlbSize)
-      .function("getGlbPtr", &RenderObject::getGlbPtr);
+EMSCRIPTEN_BINDINGS(FLTest)
+{
+    emscripten::class_<FeatureLayerRenderer>("FeatureLayerRenderer")
+        .constructor()
+        .function("render", &FeatureLayerRenderer::render);
+    emscripten::class_<RenderObject>("RenderObject")
+        .function("getGlbSize", &RenderObject::getGlbSize)
+        .function("getGlbPtr", &RenderObject::getGlbPtr);
 }
 
-#endif // ERDBLICK_FEATURELAYERRENDERER_H
+#endif  // ERDBLICK_FEATURELAYERRENDERER_H
