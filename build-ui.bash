3a7a860676aa7240b0a3ec82fa2f0ff3d9520177
Vagram Airiian <v.airiian@klebert-engineering.de>
2023-12-18 17:50:21 +0100

    Fix lines

--- build-ui.bash
+++ build-ui.bash
@@ -12,9 +12,9 @@ cd "$SOURCE_LOC" || exit 1
 echo "Collecting npm modules."
 npm install
 
-echo "Patch erblick-core TS definitions."
+echo "Patching erblick-core TS definitions."
 if grep -q "export default libErdblickCore" "$SOURCE_LOC/build/libs/core/erdblick-core.d.ts"; then
-    echo "The TS definitions are already patched."
+    echo "! The TS definitions are already patched. !"
 else
     printf "\ndeclare var libErdblickCore: any; \nexport default libErdblickCore; \n" >> "$SOURCE_LOC/build/libs/core/erdblick-core.d.ts"
 fi
