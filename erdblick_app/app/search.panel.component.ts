38a30351cc84565763e250b9f63d1703dfff4fff
Wagram Airiian <wagram.airiian@gmail.com>
2024-08-08 10:28:28 +0200

    Filter unique history values

--- erdblick_app/app/search.panel.component.ts
+++ erdblick_app/app/search.panel.component.ts
@@ -30,6 +30,7 @@ export class EnterSelectDirective {
     template: `
         <div class="search-wrapper">
             <div class="search-input">
+                <!-- Expand on dialog show and collapse on dialog hide -->
                 <textarea #textarea class="single-line" rows="1" pInputTextarea
                           [(ngModel)]="searchInputValue"
                           (click)="showSearchOverlay($event)"
@@ -391,7 +392,17 @@ export class SearchPanelComponent implements AfterViewInit {
             ...this.jumpToTargetService.jumpTargets.getValue().filter(target => target.validate(value)),
             ...this.staticTargets.filter(target => target.validate(value))
         ]
-        this.visibleSearchHistory = this.searchHistory.filter(entry => entry.input.includes(value));
+        this.visibleSearchHistory = Object.values(
+            this.searchHistory.reduce((acc, obj) => {
+                if (obj.input.includes(value)) {
+                    const key = `${obj.label}-${obj.index}-${obj.input}`;
+                    if (!acc[key]) {
+                        acc[key] = obj;
+                    }
+                }
+                return acc;
+            }, {} as Record<string, typeof this.searchHistory[number]>)
+        );
     }
 
     setSelectedMap(value: string|null) {
