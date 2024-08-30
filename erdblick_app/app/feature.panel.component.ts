dbbfe52faf3a2f40532e9b56582f909872f686ac
Johannes Wolf <mail@johannes-wolf.com>
2024-08-30 14:42:40 +0200

    Feature Inspector: Always expand section nodes

--- erdblick_app/app/feature.panel.component.ts
+++ erdblick_app/app/feature.panel.component.ts
@@ -230,8 +230,9 @@ export class FeaturePanelComponent implements OnInit  {
     expandTreeNodes(nodes: TreeTableNode[], parent: any = null): void {
         nodes.forEach(node => {
             const isTopLevelNode = parent === null;
+            const isSection = node.data["type"] === this.InspectionValueType.SECTION.value;
             const hasSingleChild = node.children && node.children.length === 1;
-            node.expanded = isTopLevelNode || hasSingleChild;
+            node.expanded = isTopLevelNode || isSection || hasSingleChild;
 
             if (node.children) {
                 this.expandTreeNodes(node.children, node);
