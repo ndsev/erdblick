fe324122aa9643e26108cd8595eaa1c3f3d937ec
Joseph Birkner <j.birkner@klebert-engineering.de>
2024-10-17 16:58:27 +0200

    Pin emscripten

--- ci/00_linux_setup.bash
+++ ci/00_linux_setup.bash
@@ -8,6 +8,8 @@ export PATH=$PATH:"$ci_dir/../node_modules/.bin/"
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git pull
-./emsdk install latest
-./emsdk activate latest
+# For some reason, emsdk>=3.1.68 leads to an error when compiling fmt
+# due to more restrictive constexpr checks.
+./emsdk install 3.1.67
+./emsdk activate 3.1.67
 source ./emsdk_env.sh
