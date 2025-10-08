4b31694c94bdd901816109274071c0e600a64d3e
Wagram Airiian <wagram.airiian@gmail.com>
2025-10-08 12:54:09 +0200

    Allow more than one view in container

--- app/mapview/view.container.component.ts
+++ app/mapview/view.container.component.ts
@@ -5,17 +5,17 @@ import {map} from "rxjs";
 @Component({
     selector: 'mapview-container',
     template: `
-        <div>
-            <ng-container *ngIf="showSplitter">
-                <p-splitter [panelSizes]="panelSizes$ | async" class="mb-8">
-                    <ng-container *ngFor="let idx of viewIndices$ | async; trackBy: trackByIndex">
+        <ng-container *ngIf="panelSizes$ | async as panelSizes">
+            <p-splitter [panelSizes]="panelSizes" class="mb-8">
+                <ng-container *ngIf="viewIndices$ | async as viewIndices">
+                    <ng-container *ngFor="let idx of viewIndices; trackBy: trackByIndex">
                         <ng-template pTemplate="panel">
                             <map-view [viewIndex]="idx"></map-view>
                         </ng-template>
                     </ng-container>
-                </p-splitter>
-            </ng-container>
-        </div>
+                </ng-container>
+            </p-splitter>
+        </ng-container>
     `,
     styles: [`
         @media only screen and (max-width: 56em) {
@@ -34,17 +34,8 @@ export class MapViewContainerComponent {
     panelSizes$ = this.stateService.numViewsState.pipe(
         map(n => Array.from({ length: n }, () => 100 / n))
     );
-    // A flag to force split-rerender
-    showSplitter = true;
 
     constructor(private stateService: AppStateService) {
-        this.stateService.numViewsState.subscribe(() => {
-            // whenever numViews changes, force a quick toggle to remount splitter
-            this.showSplitter = false;
-            setTimeout(() => {
-                this.showSplitter = true;
-            });
-        });
     }
 
     trackByIndex(_: number, i: number) {
