aa65ac177301c99e14736ec30a6e615c3be51ed5
Joseph Birkner <j.birkner@klebert-engineering.de>
2026-08-24 08:14:43 +0200

    Advance cross-platform mapget fixture fix

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -41,7 +41,7 @@ if (NOT TARGET mapget-model)
     CPMAddPackage(
       NAME mapget
       GIT_REPOSITORY "https://github.com/ndsev/mapget.git"
-      GIT_TAG "d24361ba89ab7c25d43c1bb0c9d622d0a47b813c"
+      GIT_TAG "1540a1d50bc42e60664f4ebe6e30287269b28443"
       GIT_SHALLOW OFF
       OPTIONS
         "MAPGET_WITH_WHEEL OFF"
